156dbba57e943d0b42896ad6a7d749ab9e6d4896
Jorge Vega <jgvoslp@gmail.com>
2024-05-20 12:31:42 -0400

    nuevo historial

--- cypress/e2e/smoketestbanco.cy.js
+++ cypress/e2e/smoketestbanco.cy.js
@@ -5,7 +5,7 @@ describe("Test Suite - Conjunto de Prueba",()=>{
 
       })
     it("Validar Pagina de Iniciooo", ()=>{
-        
+sev        
         cy.get(".active >img").should("be.visible")
         cy.get(".active > .custom >h4").contains("Jorge Vega")        
 
